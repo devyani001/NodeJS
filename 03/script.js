ece95d2e531c992f5f4369bd2bd6255e1eb25ba1
devyani001 <devyani140424@gmail.com>
2025-07-24 15:48:40 +0530

    day-3 hello world in js & package.json file

---
+++ 03/script.js
@@ -0,0 +1,8 @@
+console.log("hey there , i am js!");
+//console.log(window);
+
+/*
+        npm - node package manager
+        npm init -> npm initialization 
+                    ye hamare liye template/file banata h jo hmare pjt ko run krne k kaam aayegi...n
+*/
